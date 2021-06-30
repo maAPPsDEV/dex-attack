f2d93bb165f5108c6b381d8b00e91cd72a574ce6
Tony <tony@maapps.dev>
2021-06-30 10:30:48 -0400

    Improve Test with utils

--- test/hacker.js
+++ test/hacker.js
@@ -1,5 +1,7 @@
 const Hacker = artifacts.require("Hacker");
+const utils = require("./helpers/utils");
 const { expect } = require("chai");
+const { BN } = require("@openzeppelin/test-helpers");
 
 /*
  * uncomment accounts to access the test accounts made available by the
